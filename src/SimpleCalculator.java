c1b9d20d0fc12fc5bf71dd90db041a78155d7664
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-06-16 10:57:33 -0700

    simple calculator

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -1,25 +1,46 @@
 
-
 import javax.swing.JOptionPane;
 
 public class SimpleCalculator {
 	public static void main(String[] args) {
 
 		// 1. Get 2 numbers from the user and convert them to integer.
+		String N = JOptionPane.showInputDialog("Enter two numbers");
+		int number = Integer.parseInt(N);
 
 		// 2. Customize pop-up to support add/subtract/multiply/divide operations.
-		int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Button1", "Button2", "Button3", "Button3" },
-				null);
-
-		// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
-
-		// 6. Wrap steps 1, 2 and 5 in a for loop to repeat calculator operations
-	}
+		int operation = JOptionPane.showOptionDialog(null, "choose an operation", "calculator", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "add", "subtract", "multiply", "divide" }, null);
+	
+	// 5. Call the methods created in steps 3 and 4 to perform the appropriate
+	// operation.
+		if(operation==0) {
+			Add();
+		}
+		
+	// 6. Wrap steps 1, 2 and 5 in a for loop to repeat calculator operations
 
 	// 3. Create method for addition operation.
-	//    Method should create pop-up that shows equation and solution, eg. 45 + 25 = 70.
-	//    HINT: use 'static void add(int num1, num2) { ... }
+	// Method should create pop-up that shows equation and solution, eg. 45 + 25 =
+	// 70.
+	// HINT: use 'static void add(int num1, num2) { ... }
+	}
+	private static void Add(int num1, int num2) {
+		JOptionPane.showMessageDialog(null, num1 + num2);
+	}
 
 	// 4. Create similar methods for subtraction, multiplication and division.
+	private static void subtract(int num1, int num2) {
+		JOptionPane.showMessageDialog(null, num1 - num2);
+	}
+
+	private static void multiply(int num1, int num2) {
+		JOptionPane.showMessageDialog(null, num1 * num2);
+	}
+
+	private static void divide(int num1, int num2) {
+		JOptionPane.showMessageDialog(null, num1 / num2);
+
+	}
+
 }
