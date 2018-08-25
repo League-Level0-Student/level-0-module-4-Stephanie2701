3d9e7d8ca1c096882cf9628a511d81d7715c630f
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-25 10:53:20 -0700

    loops

---
+++ src/loops.java
@@ -0,0 +1,64 @@
+
+public class loops {
+	
+	public static void main(String[] args) {
+		
+		
+		//for (int i = 0; i <101 ; i++) {
+			
+		//	System.out.println(i);	
+			
+		//}
+		
+		//for (int i = 100; i >=0; i--) {
+			//System.out.println(i);
+			
+		//}
+		
+		
+		
+		//for (int i = 2; i < 100; i++) {
+			
+			//if(i%2==0) {
+			//	System.out.println(i);
+			//}
+	//	}
+		
+		///for (int i = 1; i < 99; i++) {
+		//	if(i%2== 1) {
+		//	System.out.println(i);
+		//}
+		//}
+		
+	//	for (int i = 1; i < 500; i++) {
+			
+		//	if(i%2==0) {
+		//	System.out.println(i+" is even");
+//	}else if(i%2==1) {
+		//System.out.println(i+" is odd");
+//	}
+//}
+
+	//	for (int j = 7; j < 777; j++) {
+		//	if(j%7==0) {
+			//	System.out.println(j);
+			//}
+			
+	//	}
+		
+	int num = 1++;
+	
+		
+		for (int i = 2002; i < 2019; i++) {
+			//for (int j = 1; j < 17; j++) {
+				
+			
+			System.out.println("In "+i+" I was "+num+" years old");
+			
+		}
+		
+		
+		
+	
+}
+}
